40914360ce8c74f0feaee6c9e4b77ee2c09313de
Zolteks <96941840+Zolteks@users.noreply.github.com>
2024-02-05 14:55:56 +0100

    client app

--- MorpionServeur/MorpionServeur.cpp
+++ MorpionServeur/MorpionServeur.cpp
@@ -1,20 +1,73 @@
-// MorpionServeur.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    // Initialisation de Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        //std::cerr << "Erreur lors de l'initialisation de Winsock." << std::endl;
+        return 1;
+    }
+
+    // Création du socket
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Erreur lors de la création de la socket." << std::endl;
+        WSACleanup();
+        return 1;
+    }
+    else
+    {
+        std::cerr << "socket créer" << std::endl;
+    }
+
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(80);
+
+    if (inet_addr("142.250.178.142") == INADDR_NONE) {
+        std::cerr << "Erreur lors de la conversion de l'adresse IP." << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+    serverAddress.sin_addr.s_addr = inet_addr("142.250.178.142");
+
+
+    if (connect(clientSocket, reinterpret_cast<struct sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cerr << "Erreur lors de la connexion au serveur." << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+    else {
+        std::cerr << "serveur créer" << std::endl;
+    }
+
+    //requete
+    const char* request = "GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n";
+    int stockRequest = send(clientSocket, request, strlen(request), 0);// envoie de la requete
+
+    if (stockRequest == -1)
+    {
+        printf("Erreur lors de l'envoi de la requête");
+    }
+    else {
+
+        printf("La requête a été envoyée avec succès\n");
+        char buffer[4096];
+        int revceve = recv(clientSocket, buffer, sizeof(buffer), 0);//reponse
+
+        std::cout.write(buffer, revceve);
+    }
+
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
