f061f908482bb5f7c7d7114e48387e06f584bc76
Léo <lclipet@gaming.tech>
2024-02-06 10:09:51 +0100

    Update MorpionServeur.cpp

--- MorpionServeur/MorpionServeur.cpp
+++ MorpionServeur/MorpionServeur.cpp
@@ -1,72 +1,140 @@
-#include <iostream>
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
 #include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
 
-#pragma comment(lib, "ws2_32.lib")
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
 
-int main() {
-    // Initialisation de Winsock
+int __cdecl main(void)
+{
     WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        //std::cerr << "Erreur lors de l'initialisation de Winsock." << std::endl;
+    int iResult;
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+    SOCKET ClientSocket = INVALID_SOCKET;
+
+    struct addrinfo* result = NULL;
+    struct addrinfo hints;
+
+    int iSendResult;
+    char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
         return 1;
     }
 
-    // Création du socket
-    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == INVALID_SOCKET) {
-        std::cerr << "Erreur lors de la création de la socket." << std::endl;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
         WSACleanup();
         return 1;
     }
-    else
-    {
-        std::cerr << "socket créer" << std::endl;
-    }
 
-
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(80);
-
-    if (inet_addr("142.250.178.142") == INADDR_NONE) {
-        std::cerr << "Erreur lors de la conversion de l'adresse IP." << std::endl;
-        closesocket(clientSocket);
+    // Create a SOCKET for the server to listen for client connections.
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET) {
+        printf("socket failed with error: %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
         WSACleanup();
         return 1;
     }
-    serverAddress.sin_addr.s_addr = inet_addr("142.250.178.142");
 
-
-    if (connect(clientSocket, reinterpret_cast<struct sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
-        std::cerr << "Erreur lors de la connexion au serveur." << std::endl;
-        closesocket(clientSocket);
+    // Setup the TCP listening socket
+    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
         WSACleanup();
         return 1;
     }
-    else {
-        std::cerr << "serveur créer" << std::endl;
+
+    freeaddrinfo(result);
+
+    iResult = listen(ListenSocket, SOMAXCONN);
+    if (iResult == SOCKET_ERROR) {
+        printf("listen failed with error: %d\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
     }
 
-    //requete
-    const char* request = "GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n";
-    int stockRequest = send(clientSocket, request, strlen(request), 0);// envoie de la requete
-
-    if (stockRequest == -1)
-    {
-        printf("Erreur lors de l'envoi de la requête");
+    // Accept a client socket
+    ClientSocket = accept(ListenSocket, NULL, NULL);
+    if (ClientSocket == INVALID_SOCKET) {
+        printf("accept failed with error: %d\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
     }
-    else {
+   
 
-        printf("La requête a été envoyée avec succès\n");
-        char buffer[4096];
-        int revceve = recv(clientSocket, buffer, sizeof(buffer), 0);//reponse
+    // No longer need server socket
+    closesocket(ListenSocket);
+   
+    // Receive until the peer shuts down the connection
+    do {
+        iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+        if (iResult > 0) {
+            printf("Bytes received: %d\n", iResult);
+            printf("Message received from client: %.*s\n", iResult, recvbuf);
 
-        std::cout.write(buffer, revceve);
+            // Echo the buffer back to the sender
+            iSendResult = send(ClientSocket, recvbuf, iResult, 0);
+            if (iSendResult == SOCKET_ERROR) {
+                printf("send failed with error: %d\n", WSAGetLastError());
+                closesocket(ClientSocket);
+                WSACleanup();
+                return 1;
+            }
+            printf("Bytes sent: %d\n", iSendResult);
+
+        }
+        else if (iResult == 0) {
+            printf("Connection closing by client...\n");
+            break;  // Sortir de la boucle si le client ferme la connexion
+        }
+        else {
+            printf("recv failed with error: %d\n", WSAGetLastError());//client fermé avant
+            closesocket(ClientSocket);
+            WSACleanup();
+            return 1;
+        }
+
+    } while (iResult > 0);
+
+    // shutdown the connection since we're done
+    iResult = shutdown(ClientSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown failed with error: %d\n", WSAGetLastError());
+        closesocket(ClientSocket);
+        WSACleanup();
+        return 1;
     }
 
-    closesocket(clientSocket);
+    // cleanup
+    closesocket(ClientSocket);
     WSACleanup();
 
     return 0;
