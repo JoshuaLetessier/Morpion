325bc8fb4eb38dd26eefe1b07f620b1e6b941e1b
Riuck01 <tforest@gaming.tech>
2024-02-07 13:15:26 +0100

    WINDOWS WORKING

--- Morpion/client.cpp
+++ Morpion/client.cpp
@@ -1,16 +1,39 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <tchar.h>
 #include <stdio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <iostream>
 
-#pragma comment(lib, "Ws2_32.lib")
-
+#pragma comment(lib, "ws2_32.lib")
+#pragma prefast(suppress : 28251, "Cette instance contient aucune annotation.")
 
 #define DEFAULT_PORT "27015"
 
 static int iResult = 0;
 static SOCKET ConnectSocket = INVALID_SOCKET;
+inline int client();
+inline int killClient();
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_CREATE:
+        // Exécuter le client lorsque la fenêtre est créée
+        if (client() != 0) {
+            MessageBox(hwnd, L"Erreur lors de l'exécution du client", L"Erreur", MB_OK | MB_ICONERROR);
+            PostQuitMessage(1);
+        }
+        break;
+    case WM_DESTROY:
+        // Terminer le client et quitter l'application lors de la fermeture de la fenêtre
+        killClient();
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
 
 inline int client()
 {
@@ -70,14 +93,10 @@ inline int client()
         return 1;
     }
 
-
-
     //iResult = send(ConnectSocket, messageToSend1, (int)strlen(messageToSend1), 0);
 
 
     //Sleep(5000);
-
-
 
     return 0;
 }
@@ -94,11 +113,41 @@ inline int killClient()
     // cleanup
     closesocket(ConnectSocket);
     WSACleanup();
+    return 0;
 }
 
 inline int sendData(const char data[4096])
 {
     iResult = send(ConnectSocket, data, (int)strlen(data), 0);
-   
+
+    return 0;
+}
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+    // Définir les paramètres de la fenêtre
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"MainWindowClass";
+
+    // Enregistrer la classe de fenêtre
+    RegisterClass(&wc);
+
+    // Créer la fenêtre
+    HWND hWnd = CreateWindowEx(0, L"MainWindowClass", L"Fenêtre Windows avec connexion au serveur", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
+    if (hWnd == NULL) {
+        return 1;
+    }
+
+    // Afficher la fenêtre
+    ShowWindow(hWnd, nCmdShow);
+
+    // Boucle de messages
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
