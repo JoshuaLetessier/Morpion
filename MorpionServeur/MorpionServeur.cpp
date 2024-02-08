57341b9efce161c12f6c5056ac8a302e617af8ec
Riuck01 <117281231+Riuck01@users.noreply.github.com>
2024-02-08 17:26:43 +0100

    nice

--- MorpionServeur/MorpionServeur.cpp
+++ MorpionServeur/MorpionServeur.cpp
@@ -1,5 +1,3 @@
-#undef UNICODE
-
 #define WIN32_LEAN_AND_MEAN
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
@@ -8,48 +6,17 @@
 #include <ws2tcpip.h>
 #include <stdlib.h>
 #include <stdio.h>
-<<<<<<< Updated upstream
-#include <fstream>
-//#include <json\json.h>
-//#include "..\simdjson\simdjson.h"
-#include <nlohmann\json.hpp>
-using json = nlohmann::json;
-
-#include "Serve.cpp"
-=======
 #include <conio.h>
 
->>>>>>> Stashed changes
 
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
 #pragma comment (lib, "User32.lib")
+#pragma comment (lib, "Gdi32.lib")
 // #pragma comment (lib, "Mswsock.lib")
 #define PORT 5150
 #define DATA_BUFSIZE 8192
 
-<<<<<<< Updated upstream
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "27015"
-#define ADRESS_IP "10.1.144.16"
-std::string newName = "Leo";
-
-void createJson() 
-{
-    // Using user-defined (raw) string literals
-    using namespace nlohmann::literals;
-    json save_morpion = R"(
-  { "name": "inconnu1", "victoire": false,  "date": 0 },
-  { "name": "inconnu2", "victoire": false,  "date": 0 }
-)"_json;
-}
-
-void editJson(std::string newName, bool newVictory, std::time_t newDate)
-{
-
-}
-
-=======
 // typedef definition
 
 typedef struct _SOCKET_INFORMATION {
@@ -680,10 +647,7 @@ HWND MakeWorkerWindow(void)
 
         printf("CreateWindow() is OK!\n");
 
+    ShowWindow(Window, SW_SHOW);
+    return Window;
 
->>>>>>> Stashed changes
-
-int __cdecl main(void)
-{
-    serve();
 }
