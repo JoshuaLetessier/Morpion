fbe7a9d65b1b913305ff5eb175ece190d7f00776
Léo <117282572+CLipet-Leo@users.noreply.github.com>
2024-02-09 18:26:21 +0100

    MAJ class Json

--- MorpionServeur/Json.cpp
+++ MorpionServeur/Json.cpp
@@ -18,35 +18,102 @@ Json::~Json()
 	file << save_systeme << save_morpion;
 }
 
-void Json::importListensocketJson(SOCKET newSocket)
+void Json::importListenSocketJson(SOCKET newSocket)
 {
-	save_systeme["socket_info"]["listen_socket"] = newSocket;
+	if (!save_systeme["socket_info"]["listen_socket"])
+	{
+		save_systeme["socket_info"]["listen_socket"] = newSocket;
+	}
 }
 
-void Json::importListensocketJson(SOCKET newSocket)
+void Json::importPlayerSocketJson(SOCKET newPlayerSocket)
 {
-	save_systeme["socket_info"]["listen_socket"] = newSocket;
+	if (!save_systeme["socket_info"]["player1_socket"])
+	{
+		save_systeme["socket_info"]["player1_socket"] = newPlayerSocket;
+	}
+	else if (save_systeme["socket_info"]["player1_socket"] != newPlayerSocket)
+	{
+		save_systeme["socket_info"]["player2_socket"] = newPlayerSocket;
+	}
 }
 
-void Json::importNameJson(std::string newName)
+void Json::importDateJson(std::time_t newDate)
 {
-	if (save_morpion["player1"]["client_socket"] = "#client_socket")
+	if (!save_systeme["socket_info"]["date"])
+	{
+		save_systeme["socket_info"]["date"] = newDate;
+	}
+}
+
+void Json::importName1Json(std::string newName)
+{
+	if (!save_morpion["player1"]["name"])
 	{
 		save_morpion["player1"]["name"] = newName;
 	}
-	else
+}
+
+void Json::importName2Json(std::string newName)
+{
+	if (!save_morpion["player2"]["name"])
 	{
 		save_morpion["player2"]["name"] = newName;
 	}
 }
 
-void Json::importBoolJson(bool newBool)
+void Json::importTurnJson(bool newTurn)
 {
-
+	if (!save_systeme["player_info"]["turn"])
+	{
+		save_systeme["player_info"]["turn"] = newTurn;
+	}
+	else
+	{
+		save_systeme["player_info"].erase("turn");
+		save_systeme["player_info"]["turn"] = newTurn;
+	}
 }
 
-void Json::importDateJson(std::time_t newDate)
+void Json::importMoveJson(std::string player, std::string newMove)
 {
-
+	if (!save_morpion["player1"]["victory"] && !save_morpion["player2"]["victory"])
+	{
+		if (save_morpion["player1"]["name"] == player )
+		{
+			if (!save_morpion["player1"]["move"])
+			{
+				save_morpion["player1"]["move"] = newMove;
+			}
+			else
+			{
+				save_morpion["player1"].erase("move");
+				save_morpion["player1"]["move"] = newMove;
+			}
+		}
+		if (save_morpion["player2"]["name"] == player)
+		{
+			if (!save_morpion["player2"]["move"])
+			{
+				save_morpion["player2"]["move"] = newMove;
+			}
+			else
+			{
+				save_morpion["player2"].erase("move");
+				save_morpion["player2"]["move"] = newMove;
+			}
+		}
+	}
 }
 
+void Json::importVictoryJson(bool player1Victory, bool player2Victory)
+{
+	if (!save_morpion["player1"]["victory"])
+	{
+		save_morpion["player1"]["victory"] = player1Victory;
+	}
+	if (!save_morpion["player2"]["victory"])
+	{
+		save_morpion["player2"]["victory"] = player2Victory;
+	}
+}
--- MorpionServeur/Json.hpp
+++ MorpionServeur/Json.hpp
@@ -5,15 +5,21 @@ public:
 	Json();
 	~Json();
 
-	void importListensocketJson(SOCKET newSocket);
-	void importNameJson(std::string newName);
-	void importBoolJson(bool newBool);
+	void importListenSocketJson(SOCKET newSocket);
+	void importPlayerSocketJson(SOCKET newPlayerSocket);
 	void importDateJson(std::time_t newDate);
+	void importName1Json(std::string newName);
+	void importName2Json(std::string newName);
+	void importTurnJson(bool newTurn);
+	void importMoveJson(std::string player, std::string newMove);
+	void importVictoryJson(bool player1Victory, bool player2Victory);
 
 
 private:
 	json save_systeme;
 	json save_morpion;
+	std::vector<json> player1Move = {};
+	std::vector<json> player2Move = {};
 
 };
 
