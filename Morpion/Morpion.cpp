5641281fedeb17f11f2e31877d06a1cd3d197681
JoshuaLetessier <jletessier@gaming.tech>
2024-02-09 11:08:39 +0100

    correction

--- Morpion/Morpion.cpp
+++ Morpion/Morpion.cpp
@@ -4,131 +4,241 @@
 #include "Morpion.hpp"
 #include "client.cpp"
 
+
 const int gridSize = 3;
 const int cellSize = 100;
+
+inline int client();
+inline int killClient();
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_CREATE:
+        // Exécuter le client lorsque la fenêtre est créée
+        if (client() != 0) {
+            MessageBox(hwnd, L"Erreur lors de l'exécution du client", L"Erreur", MB_OK | MB_ICONERROR);
+            PostQuitMessage(1);
+        }
+        break;
+    case WM_DESTROY:
+        // Terminer le client et quitter l'application lors de la fermeture de la fenêtre
+        killClient();
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
 
 Morpion::Morpion() : currentPlayer(Player::CircleRed) {
     // Initialiser la grille avec des valeurs par défaut
     board = std::vector<std::vector<Player>>(gridSize, std::vector<Player>(gridSize, Player::None));
 }
 
-    void Morpion::handleEvent(sf::Event& event) {
-        if (event.type == sf::Event::MouseButtonPressed) {
-            int mouseX = event.mouseButton.x / cellSize;
-            int mouseY = event.mouseButton.y / cellSize;
+void Morpion::handleEvent(sf::Event& event) {
+    if (event.type == sf::Event::MouseButtonPressed) {
+        int mouseX = event.mouseButton.x / cellSize;
+        int mouseY = event.mouseButton.y / cellSize;
 
-            // case est valide et non occupée
-            if (mouseX >= 0 && mouseX < gridSize && mouseY >= 0 && mouseY < gridSize &&
-                board[mouseY][mouseX] == Player::None) {
-                board[mouseY][mouseX] = currentPlayer;
-                switchPlayer();
+        // case est valide et non occupée
+        if (mouseX >= 0 && mouseX < gridSize && mouseY >= 0 && mouseY < gridSize &&
+            board[mouseY][mouseX] == Player::None) {
+            board[mouseY][mouseX] = currentPlayer;
+            switchPlayer();
+
+            std::string dataConvert = std::to_string(mouseX) + " " + std::to_string(mouseY);
+            const char* data = dataConvert.c_str();
+            sendData(data);
+        }
+    }
+}
+
+
+void Morpion::draw(sf::RenderWindow& window) {
+    window.clear();
+
+    // Dessiner la grille
+    for (int i = 0; i < gridSize; ++i) {
+        for (int j = 0; j < gridSize; ++j) {
+            sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
+            cell.setPosition(j * cellSize, i * cellSize);
+            cell.setOutlineThickness(2);
+            cell.setOutlineColor(sf::Color::Black);
+            window.draw(cell);
+
+            // Dessiner X ou O
+            if (board[i][j] == Player::CircleRed) {
+                drawCircleR(window, j * cellSize, i * cellSize);
+
+            }
+            else if (board[i][j] == Player::CircleBalck) {
+                drawCircle(window, j * cellSize, i * cellSize);
             }
         }
     }
 
+    window.display();
+}
 
-    void Morpion::draw(sf::RenderWindow& window) {
-        window.clear();
+bool Morpion::checkGameOver() const {
+    // Vérifier les lignes et colonnes
+    for (int i = 0; i < gridSize; ++i) {
+        if (board[i][0] != Player::None &&
+            board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+            return true;  // Victoire sur la ligne i
+        }
 
-        // Dessiner la grille
-        for (int i = 0; i < gridSize; ++i) {
-            for (int j = 0; j < gridSize; ++j) {
-                sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
-                cell.setPosition(j * cellSize, i * cellSize);
-                cell.setOutlineThickness(2);
-                cell.setOutlineColor(sf::Color::Black);
-                window.draw(cell);
+        if (board[0][i] != Player::None &&
+            board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+            return true;  // Victoire sur la colonne i
+        }
+    }
 
-                // Dessiner X ou O
-                if (board[i][j] == Player::CircleRed) {
-                    drawCircleR(window, j * cellSize, i * cellSize);
-                    
-                }
-                else if (board[i][j] == Player::CircleBalck) {
-                    drawCircle(window, j * cellSize, i * cellSize);
-                }
+    // Vérifier les diagonales
+    if (board[0][0] != Player::None &&
+        board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+        return true;  // Victoire sur la diagonale principale
+    }
+
+    if (board[0][2] != Player::None &&
+        board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+        return true;  // Victoire sur l'autre diagonale
+    }
+
+    // Vérifier l'égalité
+    for (int i = 0; i < gridSize; ++i) {
+        for (int j = 0; j < gridSize; ++j) {
+            if (board[i][j] == Player::None) {
+                return false;  // Il reste des cases vides, le jeu n'est pas égal
             }
         }
-
-        window.display();
     }
 
-    bool Morpion::checkGameOver() const {
-        // Vérifier les lignes et colonnes
-        for (int i = 0; i < gridSize; ++i) {
-            if (board[i][0] != Player::None &&
-                board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
-                return true;  // Victoire sur la ligne i
-            }
+    return true;  // Toutes les cases sont remplies, la partie est égale
+}
 
-            if (board[0][i] != Player::None &&
-                board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
-                return true;  // Victoire sur la colonne i
-            }
-        }
+void Morpion::switchPlayer() {
+    currentPlayer = (currentPlayer == Player::CircleRed) ? Player::CircleBalck : Player::CircleRed;
+}
 
-        // Vérifier les diagonales
-        if (board[0][0] != Player::None &&
-            board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-            return true;  // Victoire sur la diagonale principale
-        }
+void Morpion::drawCircleR(sf::RenderWindow& window, float x, float y) {
+    sf::CircleShape circle(cellSize / 2 - 10);
+    circle.setPosition(x + 10, y + 10);
+    circle.setOutlineThickness(2);
+    circle.setOutlineColor(sf::Color::Red);
+    circle.setFillColor(sf::Color::Transparent);
+    window.draw(circle);
+}
 
-        if (board[0][2] != Player::None &&
-            board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-            return true;  // Victoire sur l'autre diagonale
-        }
+void Morpion::drawCircle(sf::RenderWindow& window, float x, float y) {
+    sf::CircleShape circle(cellSize / 2 - 10);
+    circle.setPosition(x + 10, y + 10);
+    circle.setOutlineThickness(2);
+    circle.setOutlineColor(sf::Color::Black);
+    circle.setFillColor(sf::Color::Transparent);
+    window.draw(circle);
+}
 
-        // Vérifier l'égalité
-        for (int i = 0; i < gridSize; ++i) {
-            for (int j = 0; j < gridSize; ++j) {
-                if (board[i][j] == Player::None) {
-                    return false;  // Il reste des cases vides, le jeu n'est pas égal
-                }
-            }
-        }
+//std::string getPlayerName() {
+//    sf::RenderWindow nameWindow(sf::VideoMode(400, 200), "Entrez votre nom");
+//    sf::Font font;
+//    if (!font.loadFromFile("C:\\Users\\Faoll\\Downloads\\namecat\\Namecat.ttf")) {
+//        return "erreur font";  // Ou une autre valeur d'erreur si nécessaire
+//    }
+//
+//
+//    sf::Text text;
+//    text.setFont(font);
+//    text.setCharacterSize(24);
+//    text.setFillColor(sf::Color::White);
+//    text.setPosition(50, 50);
+//    text.setString("Entrez votre nom : ");
+//
+//    sf::String inputText;
+//    sf::Text inputDisplay;
+//    inputDisplay.setFont(font);
+//    inputDisplay.setCharacterSize(24);
+//    inputDisplay.setFillColor(sf::Color::White);
+//    inputDisplay.setPosition(50, 100);
+//
+//    while (nameWindow.isOpen()) {
+//        sf::Event event;
+//        while (nameWindow.pollEvent(event)) {
+//            if (event.type == sf::Event::Closed) {
+//                nameWindow.close();
+//            }
+//            else if (event.type == sf::Event::TextEntered) {
+//                if (event.text.unicode == 8 && !inputText.isEmpty()) { // Backspace
+//                    inputText.erase(inputText.getSize() - 1, 1);
+//                }
+//                else if (event.text.unicode >= 32 && event.text.unicode < 128) {
+//                    inputText += event.text.unicode;
+//                }
+//                inputDisplay.setString(inputText);
+//            }
+//        }
+//
+//        nameWindow.clear();
+//        nameWindow.draw(text);
+//        nameWindow.draw(inputDisplay);
+//        nameWindow.display();
+//    }
+//
+//    return inputText.toAnsiString();
+//}
 
-        return true;  // Toutes les cases sont remplies, la partie est égale
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+    // Définir les paramètres de la fenêtre
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"MainWindowClass";
+
+    // Enregistrer la classe de fenêtre
+    RegisterClass(&wc);
+
+    // Créer la fenêtre
+    HWND hWnd = CreateWindowEx(0, L"MainWindowClass", L"Fenêtre Windows avec connexion au serveur", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
+    if (hWnd == NULL) {
+        return 1;
     }
 
-    void Morpion::switchPlayer() {
-        currentPlayer = (currentPlayer == Player::CircleRed) ? Player::CircleBalck : Player::CircleRed;
-    }
+    // Afficher la fenêtre
+    ShowWindow(hWnd, nCmdShow);
 
-    void Morpion::drawCircleR(sf::RenderWindow& window, float x, float y) {
-        sf::CircleShape circle(cellSize / 2 - 10);
-        circle.setPosition(x + 10, y + 10);
-        circle.setOutlineThickness(2);
-        circle.setOutlineColor(sf::Color::Red);
-        circle.setFillColor(sf::Color::Transparent);
-        window.draw(circle);
+    // Boucle de messages
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
-
-    void Morpion::drawCircle(sf::RenderWindow& window, float x, float y) {
-        sf::CircleShape circle(cellSize / 2 - 10);
-        circle.setPosition(x + 10, y + 10);
-        circle.setOutlineThickness(2);
-        circle.setOutlineColor(sf::Color::Black);
-        circle.setFillColor(sf::Color::Transparent);
-        window.draw(circle);
-    }
-
-int main() {
 
     client();
 
-    std::string player1Name;
-    std::string player2Name;
+    //std::string player1Name;
+    //std::string player2Name;
 
-    std::cout << "Entrez le nom du Joueur 1 (X) : ";
-    std::cin >> player1Name;
+    //std::cout << "Entrez le nom du Joueur 1 (X) : ";
+    //std::cin >> player1Name;
 
-    sendData(player1Name.c_str());
+    //sendData(player1Name.c_str());
 
-    std::cout << "Entrez le nom du Joueur 2 (O) : ";
-    std::cin >> player2Name;
+    //std::cout << "Entrez le nom du Joueur 2 (O) : ";
+    //std::cin >> player2Name;
 
-    sendData(player2Name.c_str());
+    //sendData(player2Name.c_str());
 
+ /*   std::string player1Name = getPlayerName();
+    if (player1Name.empty()) {
+        MessageBox(NULL, L"Nom de joueur non valide. Fermeture de l'application.", L"Erreur", MB_OK | MB_ICONERROR);
+        return 1;
+    }
+
+    sendData(player1Name.c_str());*/
+
+
+    
 
 
     sf::RenderWindow window(sf::VideoMode(gridSize * cellSize, gridSize * cellSize), "Morpion Joueur contre Joueur");
@@ -143,15 +253,18 @@ int main() {
                 killClient();
             }
 
+            
+
             game.handleEvent(event);
         }
-
+       
         game.draw(window);
         if (game.checkGameOver()) {
             std::cout << "La partie est terminée !" << std::endl;
             window.close();
         }
     }
+
 
     return 0;
 }
--- Morpion/client.cpp
+++ Morpion/client.cpp
@@ -1,43 +1,22 @@
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <tchar.h>
 #include <stdio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <iostream>
 
-#pragma comment(lib, "ws2_32.lib")
-#pragma prefast(suppress : 28251, "Cette instance contient aucune annotation.")
+#pragma comment(lib, "Ws2_32.lib")
 
+#define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "27015"
-#define ADRESS_IP "192.168.1.25"
+#define ADRESS_IP "127.0.0.1"
 static int iResult = 0;
 static SOCKET ConnectSocket = INVALID_SOCKET;
-inline int client();
-inline int killClient();
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-    case WM_CREATE:
-        // Exécuter le client lorsque la fenêtre est créée
-        if (client() != 0) {
-            MessageBox(hwnd, L"Erreur lors de l'exécution du client", L"Erreur", MB_OK | MB_ICONERROR);
-            PostQuitMessage(1);
-        }
-        break;
-    case WM_DESTROY:
-        // Terminer le client et quitter l'application lors de la fermeture de la fenêtre
-        killClient();
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);
-    }
-    return 0;
-}
 
 inline int client()
 {
+
     WSADATA wsaData;
+
 
     struct addrinfo* result = NULL;
     struct addrinfo* ptr = NULL;
@@ -47,60 +26,91 @@ inline int client()
     char recvbuf[DEFAULT_BUFLEN];
     int recvbuflen = DEFAULT_BUFLEN;
 
+
+
+
     // Initialize Winsock 
     iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0) {
-
         printf("WSAStartup failed with error: %d\n", iResult);
         return 1;
     }
 
     ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
+    hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
 
     // Resolve the server address and port
-    iResult = getaddrinfo("127.0.0.1", DEFAULT_PORT, &hints, &result);
+    iResult = getaddrinfo(ADRESS_IP, DEFAULT_PORT, &hints, &result);
     if (iResult != 0) {
         printf("getaddrinfo failed with error: %d\n", iResult);
         WSACleanup();
         return 1;
     }
 
+    ptr = result;
     // Attempt to connect to an address until one succeeds
-    for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+    //for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
 
         // Create a SOCKET for connecting to server
-        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-        if (ConnectSocket == INVALID_SOCKET) {
-            printf("socket failed with error: %ld\n", WSAGetLastError());
-            WSACleanup();
-            return 1;
-        }
-
-        // Connect to server
-        iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
-        if (iResult == SOCKET_ERROR) {
-            closesocket(ConnectSocket);
-            ConnectSocket = INVALID_SOCKET;
-            continue;
-        }
-        break;
-    }
-
-    freeaddrinfo(result);
-
+    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
     if (ConnectSocket == INVALID_SOCKET) {
-        printf("Unable to connect to server!\n");
+        printf("socket failed with error: %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
         WSACleanup();
         return 1;
     }
 
-    //iResult = send(ConnectSocket, messageToSend1, (int)strlen(messageToSend1), 0);
+    // Connect to server
+    iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        printf("Error for socket server : %ld\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        ConnectSocket = INVALID_SOCKET;
+        //continue;
+    }
+    //break;
+// }
+
+    freeaddrinfo(result);
+
+    printf("avant test connexion...\n");
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("Unable to connect to server! %ld\n", WSAGetLastError());
+       
+        WSACleanup();
+        return 1;
+    }
 
 
-    //Sleep(5000);
+    const char* data = "connexion";
+    iResult = send(ConnectSocket, data, (int)strlen(data), 0);
+    if (iResult == SOCKET_ERROR) {
+        printf("send failed: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+
+
+    iResult = send(ConnectSocket, data, (int)strlen(data), 0);
+
+
+    iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+    if (iResult > 0) {
+        printf("Message received from server: %.*s\n", iResult, recvbuf);
+    }
+    else if (iResult == 0) {
+        printf("Connection closed by server...\n");
+    }
+    else {
+        printf("recv failed with error: %d\n", WSAGetLastError());
+    }
+
+
 
     return 0;
 }
@@ -114,44 +124,15 @@ inline int killClient()
         WSACleanup();
         return 1;
     }
-    // cleanup
+
     closesocket(ConnectSocket);
     WSACleanup();
-    return 0;
 }
 
 inline int sendData(const char data[4096])
 {
+
     iResult = send(ConnectSocket, data, (int)strlen(data), 0);
-
-    return 0;
-}
-
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
-    // Définir les paramètres de la fenêtre
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = L"MainWindowClass";
-
-    // Enregistrer la classe de fenêtre
-    RegisterClass(&wc);
-
-    // Créer la fenêtre
-    HWND hWnd = CreateWindowEx(0, L"MainWindowClass", L"Fenêtre Windows avec connexion au serveur", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
-    if (hWnd == NULL) {
-        return 1;
-    }
-
-    // Afficher la fenêtre
-    ShowWindow(hWnd, nCmdShow);
-
-    // Boucle de messages
-    MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
 
     return 0;
 }
